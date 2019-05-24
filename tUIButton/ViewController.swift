857cce1db1c2940ffb74609faa0a7078ce6cf475
tyobigoro <n.n.6.suke@gmail.com>
2019-05-25 00:33:07 +0900

    c

--- tUIButton/ViewController.swift
+++ tUIButton/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tBtn: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //tBtn.imageView?.contentMode = .scaleAspectFit
+        //tBtn.contentHorizontalAlignment = .fill
+        //tBtn.contentVerticalAlignment = .fill
     }
-
-
 }
 
+class CustomBtn: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super .init(coder: aDecoder)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        print("awakeFromNib")
+        // UIButtonサイズは width:44, height:30 に固定
+        // UIButton配置は viewSafeAreaのHcenter/Vcenter
+        // ここへの記述が的確なのか？
+        self.imageView?.contentMode = .scaleAspectFit
+        self.contentHorizontalAlignment = .fill
+        self.contentVerticalAlignment = .fill
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+}
